e93e9f34cb95b4a6b39caed2eed6a5c6c38ac03b
Neekon Saadat <flygy2003@users.noreply.github.com>
2017-06-25 14:06:11 -0700

    johnny5.js!

--- hub.js
+++ hub.js
@@ -1,9 +1,9 @@
 var io = require('johnny-five.js')
-var board = new five.Board();
+var board = new io.Board();
 
 board.on("ready", function() {
   // Create a standard `led` component instance
-  var led = new five.Led(13);
+  var led = new io.Led(13);
   // "blink" the led in 500ms
   // on-off phase periods
   led.blink(500);
--- src/js/components/Cards.jsx
+++ src/js/components/Cards.jsx
@@ -1,7 +1,18 @@
 import React, {Component} from "react";
+var io = require('johnny-five.js')
+
 
 class Cards extends Component {
   render() {
+    var board = new io.Board();
+
+    board.on("ready", function() {
+      // Create a standard `led` component instance
+      var led = new io.Led(13);
+      // "blink" the led in 500ms
+      // on-off phase periods
+      led.blink(500);
+    });
     return (
         <div id="wrapper-div">
           <Edit/>
